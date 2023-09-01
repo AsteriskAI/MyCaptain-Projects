d82f05477c259d97b73b652b8857b9c2f1d2b06c
Zaid Omar <136264653+AsteriskAI@users.noreply.github.com>
2023-09-01 18:42:27 +0400

    Completed loop project (print positive numbers)

---
+++ MyCaptain/Project2.py
@@ -0,0 +1,13 @@
+list1 = [12,-7,5,64,-14]
+
+for x in list1:
+    if x < 0:
+        continue
+    print(x)
+
+list2 = [12,14,-95,3]
+
+for y in list2:
+    if y < 0:
+        continue
+    print(y)
